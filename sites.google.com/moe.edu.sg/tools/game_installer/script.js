6dea6b27497b59d18e3b866e70c34813069d5ab5
coconutc69 <138122019+coconutc69@users.noreply.github.com>
2026-08-21 20:16:50 +0800

    i think i forgot to put ; on some javascript and also im making a button that you have to click

--- sites.google.com/moe.edu.sg/tools/game_installer/script.js
+++ sites.google.com/moe.edu.sg/tools/game_installer/script.js
@@ -1,5 +1,7 @@
-document.body.innerHTML = `<h1>game installer thingy</h1><br><p>click next to continue</p><br><button onclick="page2()">`
+function page1() {
+    document.body.innerHTML = `<h1>game installer thingy</h1><br><p>click next to continue</p><br><button onclick="page2()">`;
+}
 
 function page2() {
-    document.body.innerHTML = `<h1>select a game</h1><br><p>select a game from the list and upload the install file for the game</p>`
+    document.body.innerHTML = `<h1>select a game</h1><br><p>select a game from the list and upload the install file for the game</p>`;
 }
