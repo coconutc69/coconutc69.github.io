e92f51c0783c429bf557cf078294dd63d7c882ee
coconutc69 <138122019+coconutc69@users.noreply.github.com>
2026-08-21 17:58:09 +0800

    cleaned up some stuff and temporarily disabled some stuff

---
+++ sites.google.com/moe.edu.sg/script.js
@@ -0,0 +1,11 @@
+function tempdisabled() {
+    document.body.innerHTML = `<h1>this function has been temporarily disabled</h1>`;
+}
+
+function launch120game() {
+    window.open(
+        "https://coconutc69.github.io/sites.google.com/moe.edu.sg/games/eaglercraft/eaglercraft120.html",
+        "popup",
+        "width=800,height=600,resizable=no,scrollbars=yes"
+    );
+}
